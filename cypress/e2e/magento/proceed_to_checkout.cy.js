69a72b133bc56674c2234676617aced5cc506f53
Raihan Kemal Arsyad <132347723+Eang08@users.noreply.github.com>
2025-02-22 20:19:43 +0700

    Fixied Procced to Checkout

--- cypress/e2e/magento/proceed_to_checkout.cy.js
+++ cypress/e2e/magento/proceed_to_checkout.cy.js
@@ -2,7 +2,7 @@ describe('Proceed to Checkout', () => {
   beforeEach(() => {
     cy.visit('https://magento.softwaretestingboard.com/')
   })
-  it.skip('Succsesfuly Proceed', () => {
+  it('Succsesfuly Proceed', () => {
     cy.get('.panel > .header > .authorization-link > a').click()
     cy.url().should('include','login')
     cy.get('#email').type('kelompok9_sanber_sqa@gmail.com')
